55c1bad596a053a87242ef45c476215d6ef49d70
Dan Banfield <dandbanfield@gmail.com>
2022-09-25 20:24:13 +0100

    Add project files.

---
+++ BFSTM Reader/BFSTMFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace BFSTM_Reader
+{
+    internal class BFSTMFile : IDisposable
+    {
+        private const long MAGIC_OFFSET = 0x00;
+
+        private FileStream fileStream;
+        private BinaryReader binaryReader;
+
+        internal BFSTMFile(string fileLocation)
+        {
+            fileStream = new FileStream(fileLocation, FileMode.Open);
+            binaryReader = new BinaryReader(fileStream);
+        }
+
+        internal char[] MagicBytes()
+        {
+            fileStream.Seek(MAGIC_OFFSET, SeekOrigin.Begin);
+            return binaryReader.ReadChars(4);
+        }
+
+        public void Dispose()
+        {
+            binaryReader.Dispose();
+            fileStream.Dispose();
+        }
+    }
+}
---
+++ BFSTM Reader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BFSTM_Reader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetUpConsole();
+
+            if (args.Length == 0 || args == null)
+            {
+                AskForFileLocation();
+                return;
+            }
+
+            DisplayFileInfo(args[0]);
+        }
+
+        static void SetUpConsole()
+        {
+            Console.Title = "BFSTM Reader";
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void AskForFileLocation()
+        {
+            Console.Write("What's the location of the BFSTM file? ");
+            string fileLocation = Console.ReadLine();
+            DisplayFileInfo(fileLocation);
+        }
+
+        static void DisplayFileInfo(string fileLocation)
+        {
+            Console.Clear();
+            using (BFSTMFile bfstmFile = new BFSTMFile(fileLocation))
+            {
+                Console.Write("Magic Bytes: " + new string(bfstmFile.MagicBytes()));
+            }
+            Console.ReadKey();
+        }
+    }
+}
